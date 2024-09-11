3b6cee5650771577c5568432a68d3f0a93f3bb74
amjadtaleb <amjadtaleb@gmail.com>
2024-09-11 13:59:32 +0200

    add get article by id entrypoint

--- orders/inventory/api.py
+++ orders/inventory/api.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from django.db.models import F
 from ninja import Router
+from ninja.errors import HttpError
 
 from .models import PricedArticle
 from .schemas import ArticleSchema
@@ -17,3 +18,20 @@ def list_articles(request, offset: int = 0, limit: Optional[int] = None):
         name=F("article__name"),
         description=F("article__description"),
     ).values()[slice(offset, limit)]
+
+
+@router.get("/article/{id}", response=ArticleSchema)
+def get_article(request, id: int):
+    if (
+        article := PricedArticle.recents.annotate(
+            quantity=F("article__inventoryarticle__quantity"),
+            date_created=F("article__date_created"),
+            reference=F("article__reference"),
+            name=F("article__name"),
+            description=F("article__description"),
+        )
+        .filter(article_id=id)
+        .first()
+    ):
+        return article
+    raise HttpError(404, "Not found")
